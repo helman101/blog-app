832a1fe57f125f68eaa5c0f76159032db8dcb871
helman101 <helmanandresortegon@gmail.com>
2021-01-15 15:13:49 +0100

    update Create action and add strong parameters method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,12 +13,18 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
     else
       render :new
     end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
